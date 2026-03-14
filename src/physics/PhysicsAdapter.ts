88ecd92f504c5dd11548d3097acfa31784eefaa7
Oscarj-jqt <jacquetoscar0@gmail.com>
2026-03-14 14:29:52 +0100

    feat(physics): add 3D, mass, angle properties to contrat Adapter

--- src/physics/PhysicsAdapter.ts
+++ src/physics/PhysicsAdapter.ts
@@ -1,10 +1,20 @@
 export type BodyId = string;
 
+// plan (x, z) et verticale y
+export type Vec3 = { x: number; y: number; z: number };
+
 export interface BodyOptions {
   id?: BodyId;
+  position?: Vec3;
+  rotation?: Vec3; 
+  linearDamping?: number;
+  angularDamping?: number;
+
   x?: number;
   y?: number;
+  z?: number;
   angle?: number;
+  
   mass?: number;
   isStatic?: boolean;
 }
