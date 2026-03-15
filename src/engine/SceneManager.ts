909611fe634c9ce9f1d725c18748ae685ec89e61
vacodevacode <vacode@outlook.fr>
2026-03-15 22:19:14 +0100

    feat(engine): add base lighting system

--- src/engine/SceneManager.ts
+++ src/engine/SceneManager.ts
@@ -37,6 +37,8 @@ export class SceneManager {
     this.renderer = new THREE.WebGLRenderer({ antialias });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     this.container.appendChild(this.renderer.domElement);
 
     if (showStats) {
