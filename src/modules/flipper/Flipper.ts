cbb29f7f1b07debf49bba5ef41e99c33fb70d649
vacodevacode <vacode@outlook.fr>
2026-03-22 00:17:05 +0100

    feat(flippers): ajout revolute joint avec limites de rotation

--- src/modules/flipper/Flipper.ts
+++ src/modules/flipper/Flipper.ts
@@ -7,7 +7,7 @@ export class Flipper {
   collider: RAPIER.Collider;
 
   constructor(world: RAPIER.World) {
-
+ 
     const geometry = new THREE.BoxGeometry(2, 0.3, 0.5);
     const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
 
@@ -17,7 +17,7 @@ export class Flipper {
     this.mesh.castShadow = true;
     this.mesh.receiveShadow = true;
 
-    // Ici on gére la physique
+    // Ici on gere la physique
 
     // RigidBody kinematic
     const rigidBodyDesc = RAPIER.RigidBodyDesc
@@ -29,6 +29,29 @@ export class Flipper {
     // Collider
     const colliderDesc = RAPIER.ColliderDesc.cuboid(1, 0.15, 0.25);
     this.collider = world.createCollider(colliderDesc, this.rigidBody);
+
+    // Ici on gere la rotation
+
+    // Ajout du pivot
+    const pivotDesc = RAPIER.RigidBodyDesc
+      .fixed()
+      .setTranslation(0, 0.5, 0);
+
+    const pivot = world.createRigidBody(pivotDesc);
+
+    // Ajout RevoluteJoint
+    const jointData = RAPIER.JointData.revolute(
+      { x: 0, y: 0, z: 0 },
+      { x: 0, y: 0, z: 0 },
+      { x: 0, y: 1, z: 0 }
+    );
+
+    // Ici on ajoute les limites d’angle
+    jointData.limitsEnabled = true;
+    jointData.limits = [-0.5, 0.5];
+
+    // Ici on crée le joint
+    world.createImpulseJoint(jointData, pivot, this.rigidBody, true);
   }
 
   addTo(scene: THREE.Scene) {
